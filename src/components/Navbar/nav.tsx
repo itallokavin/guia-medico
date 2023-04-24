47a4e665b886961d173ed7e607f6d9699b2d55f1
Itallo Kavin <italo.keven50@gmail.com>
2023-04-24 18:44:47 -0300

    first commit

---
+++ src/components/Navbar/nav.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Logo from "../../../assets/img/logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+export default function Nav(){
+    return(
+        <div className="bg-verdeUnimed h-[80px] flex items-center justify-between px-16 max-[500px]:px-8">
+          <div className="hidden max-[768px]:block">
+            <FontAwesomeIcon icon={faBars} style={{fontSize: 30}} color="white" shake  />
+          </div>
+          <div className="w-[150px] max-[768px]:w-[120px]">
+            <Image alt="Unimed Logo" src={Logo}/>
+          </div>
+          <div className="max-[768px]:hidden">
+            <h2 className="font-UnimedSlab text-white text-xl font-bold tracking-[.09em]">Guia Médico</h2>
+          </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        verdeUnimed: '#00985c',
+        laranjaUnimed: '#F37920'
+      },
+      fontFamily: {
+        'UnimedSans' : 'UnimedSans',
+        'UnimedSlab' : 'UnimedSlab'
       },
     },
   },
